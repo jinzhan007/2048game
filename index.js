9eb1528a037027e8baf4a330b754a4a2f5f6c7ba
jinzhan <420466986@qq.com>
2017-06-10 14:45:31 +0800

    2014game

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ basicText.y = app.renderer.height / 6;
 
 app.stage.addChild(basicText);
 
-
 var grid = [];
 for (var i = 0; i < 4; i++){
     grid[i] = [0,0,0,0];
@@ -20,9 +19,27 @@ for (var i = 0; i < 4; i++){
     for (var j = 0; j < 4; j++){
         var graphics = new PIXI.Graphics();
         graphics.beginFill(0xFF700B, 1);
-        graphics.drawRect(app .renderer.width / 6 + j * 60, app .renderer.height / 3  + i * 60, 50, 50);//x, y,width, height
+        graphics.drawRect(app.renderer.width / 8 + j * 77, app.renderer.height / 3  + i * 77, 75, 75);//x, y,width, height
         app.stage.addChild(graphics);
     }
 }
 
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 4);
+}
 
+var x = generateRandomNumber();
+var y = generateRandomNumber();
+
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0000, 1);
+graphics.drawRect(app.renderer.width / 8 + x * 77, app.renderer.height / 3  + y * 77, 75, 75);//x, y,width, height
+app.stage.addChild(graphics);
+
+var number = new PIXI.Text('2',{
+    fontSize:30
+})
+number.anchor.set(0.5);
+number.x = 75 / 2 + app.renderer.width / 8 + x * 77;
+number.y = 75 / 2 + app.renderer.height / 3 + y * 77;
+app.stage.addChild(number);
