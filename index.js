fd825825c812bd70c9649fd613d9ae952978ce7a
jinzhan <420466986@qq.com>
2017-06-10 15:25:38 +0800

    chonggou

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});//对象
 document.body.appendChild(app.view);//dom文档对象模型 接口 追加一个子元素 界面
-
 var style = new PIXI.TextStyle({
     fontFamily: 'Arial',
     fontSize: 36,
@@ -17,14 +16,11 @@ var style = new PIXI.TextStyle({
     wordWrap: true,
     wordWrapWidth: 440
 });
-
 var basicText = new PIXI.Text('2048Game',style)
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 6;
 app.stage.addChild(basicText);
-
-
 
 var grid = [];
 for (var i = 0; i < 4; i++){
@@ -44,18 +40,18 @@ function generateRandomNumber(){
     return Math.floor(Math.random() * 4);
 }
 
-var x = generateRandomNumber();
-var y = generateRandomNumber();
-
+var columnIndex = generateRandomNumber();
+var rowIndex = generateRandomNumber();
 var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFF0000, 1);
-graphics.drawRect(app.renderer.width / 8 + x * 77, app.renderer.height / 3  + y * 77, 75, 75);//x, y,width, height
+graphics.beginFill(0xFF0000, 1);//16jinzhi GRB ARGB
+graphics.drawRect(app.renderer.width / 8 + rowIndex * 77, app.renderer.height / 3  + columnIndex * 77, 75, 75);//x, y,width, height
 app.stage.addChild(graphics);
 
 var number = new PIXI.Text('2',{
     fontSize:30
 })
 number.anchor.set(0.5);
-number.x = 75 / 2 + app.renderer.width / 8 + x * 77;
-number.y = 75 / 2 + app.renderer.height / 3 + y * 77;
+number.x = 75 / 2 + app.renderer.width / 8 + rowIndex * 77;
+number.y = 75 / 2 + app.renderer.height / 3 + columnIndex * 77;
 app.stage.addChild(number);
+
