26e8657e6eb6c441d9226ad4513201732e0faebe
jinzhan <420466986@qq.com>
2017-06-10 20:03:06 +0800

    movecelltoright3

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ var style = new PIXI.TextStyle({
     wordWrap: true,
     wordWrapWidth: 440
 });
-var basicText = new PIXI.Text('2048 game',style)
+var maxCount = 16;
+var currentCount = 0;
+
+var score = 0;
+
+var basicText = new PIXI.Text('2048 game',style);
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 6;
-
 app.stage.addChild(basicText);
+
+var scoreText = new PIXI.Text('Score:' + score,style);
+basicText.anchor.set(0.5);
+basicText.x = app.renderer.width / 2;
+basicText.y = app.renderer.height / 6;
+app.stage.addChild(scoreText);
 
 var grid = [];
 for (var i = 0; i < 4; i++){
@@ -34,6 +44,7 @@ var flushUI = function () {
             drawCell(i,j);
         }
     }
+    scoreText.text = 'Score: ' + score;//
 };
 flushUI();
 
@@ -74,14 +85,17 @@ function getColorByNumber(number) {
 }
 
 var addRandomCell = function () {
+    if (currentCount === maxCount) return;
+
     var rowIndex = generateRandomNumber();
     var columnIndex = generateRandomNumber();
 
     while (grid[rowIndex][columnIndex] !== 0){
-        rowIndex = generateRandomNumber;
-        columnIndex = generateRandomNumber;
+        rowIndex = generateRandomNumber();
+        columnIndex = generateRandomNumber();
     }
     grid[rowIndex][columnIndex] = 2;
+    currentCount++;
 };
 
 addRandomCell();
@@ -89,36 +103,89 @@ addRandomCell();
 
 flushUI();
 
+var onToRightEventHandler = function () {
+    var isChanged = moveCellToRight();
+    if(isChanged){
+        addRandomCell();
+    }
+    flushUI();
+    if(checkGameOver()){
+        alert('Game Over.');
+    }
+};
+var onToLeftEventHandler = function () {
+    rotateArray(2);
+    var isChanged = moveCellToRight();
+    rotateArray(2);
+    if(isChanged){
+        addRandomCell();
+    }
+    flushUI();
+    if(checkGameOver()){
+        alert('Game Over.');
+    }
+};
+var onToUpEventHandler = function () {
+    rotateArray(1);
+    var isChanged = moveCellToRight();
+    rotateArray(3);
+    if(isChanged){
+        addRandomCell();
+    }
+    flushUI();
+    if(checkGameOver()){
+        alert('Game Over.');
+    }
+};
+var onToDownEventHandler = function () {
+    rotateArray(3);
+    var isChanged = moveCellToRight();
+    rotateArray(1);
+    if(isChanged){
+        addRandomCell();
+    }
+    flushUI();
+    if(checkGameOver()){
+        alert('Game Over.');
+    }
+};
+
 document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight'){
-       addRandomCell();
-       moveCellToRight();
-       flushUI();
+        onToRightEventHandler();
     }
     if (event.key === 'ArrowLeft'){
-        rotateArray(2);
-        moveCellToRight();
-        rotateArray(2);
-        addRandomCell();
-        flushUI();
+        onToLeftEventHandler();
     }
     if (event.key === 'ArrowUp'){
-        rotateArray(1);
-        moveCellToRight();
-        rotateArray(3);
-        addRandomCell();
-        flushUI();
+        onToUpEventHandler();
     }
     if (event.key === 'ArrowDown'){
-        rotateArray(3);
-        moveCellToRight();
-        rotateArray(1);
-        addRandomCell();
-        flushUI();
+        onToDownEventHandler();
     }
 });
 
+var hammertime = new Hammer.Manager(document, {
+    recognizers: [
+        [Hammer.Swipe, {direction: Hammer.DIRECTION_ALL}]
+    ]
+});
+hammertime.on('swiperight', function() {
+    onToRightEventHandler();
+});
+hammertime.on('swipeup', function () {
+    onToUpEventHandler();
+});
+hammertime.on('swipeleft', function () {
+    onToLeftEventHandler();
+});
+hammertime.on('swipedown', function () {
+    onToDownEventHandler();
+});
+
 function moveCellToRight() {
+    var isChanged = false;
+
     for (var rowIndex = 0; rowIndex < 4; rowIndex++){
         for (var columnIndex = 2; columnIndex >= 0; columnIndex--){
             if (grid[rowIndex][columnIndex] === 0)
@@ -128,15 +195,23 @@ function moveCellToRight() {
             if (theEmptyCellIndex !== -1) {
                 grid[rowIndex][theEmptyCellIndex] = grid[rowIndex][columnIndex];//2  0->0  2
                 grid[rowIndex][columnIndex] = 0;
+                isChanged = true;
             }
             var currentIndex = theEmptyCellIndex === -1 ? columnIndex : theEmptyCellIndex;//-1：右边没有0；
 
             if (grid[rowIndex][currentIndex] === grid[rowIndex][currentIndex + 1]){
                     grid[rowIndex][currentIndex + 1] += grid[rowIndex][currentIndex ];
                     grid[rowIndex][currentIndex] = 0;
+
+                    score += grid[rowIndex][currentIndex + 1];
+
+                    isChanged = true;
+
+                    currentCount--;
                 }
             }
         }
+        return isChanged;
 }
 
 function findTheFirstRightCell(rowIndex, columnIndex) {
@@ -160,4 +235,22 @@ function rotateArray(rotateCount = 1) {
     })
     })
     }
+}
+
+function checkGameOver() {
+    if (currentCount !== maxCount) return false;
+
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (grid[i][j] === grid[i][j - 1] ||
+                grid[i][j] === grid[i][j + 1] ||
+                (grid[i-1] && grid[i][j] === grid[i - 1][j]) ||
+                (grid[i+1] && grid[i][j] === grid[i + 1][j])
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
