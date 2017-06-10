9b9da9ef6127dd0c1a8752f87ef610ed5bbfa0d1
jinzhan <420466986@qq.com>
2017-06-10 14:53:22 +0800

    2014game

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});//对象
 document.body.appendChild(app.view);//dom文档对象模型 接口 追加一个子元素 界面
 
-var basicText = new PIXI.Text('2048Game',{
-    fontSize:50
-})
+var style = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+var basicText = new PIXI.Text('2048Game',style)
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 6;
-
 app.stage.addChild(basicText);
+
+
 
 var grid = [];
 for (var i = 0; i < 4; i++){
