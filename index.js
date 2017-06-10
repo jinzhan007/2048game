c3654d1039dfc325672caa0e72cb3421acb650ee
jinzhan <420466986@qq.com>
2017-06-10 17:11:53 +0800

    movecelltoright

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ var basicText = new PIXI.Text('2048 game',style)
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 6;
+
 app.stage.addChild(basicText);
 
 var grid = [];
@@ -27,24 +28,22 @@ for (var i = 0; i < 4; i++){
     grid[i] = [0,0,0,0];
 }
 
-for (var i = 0; i < 4; i++){
-    for (var j = 0; j < 4; j++){
-        drawCell(i,j);
+var flushUI = function () {
+    for (var i = 0; i < 4; i++){
+        for (var j = 0; j < 4; j++){
+            drawCell(i,j);
+        }
     }
-}
+};
+flushUI();
 
 function generateRandomNumber(){
     return Math.floor(Math.random() * 4);
 }
 
 function drawCell(rowIndex, columnIndex) {
-    var color = 0xFF0000;
-    if (grid[rowIndex][columnIndex] === 2){
-        color = 0xFF700B;
-    }
-
     var graphics = new PIXI.Graphics();
-    graphics.beginFill(color, 1);
+    graphics.beginFill(getColorByNumber(grid[rowIndex][columnIndex]), 1);
     graphics.drawRect(app.renderer.width / 8 + columnIndex * 77, app.renderer.height / 3  + rowIndex * 77, 75, 75);//x, y,width, height
     app.stage.addChild(graphics);
 
@@ -59,6 +58,16 @@ function drawCell(rowIndex, columnIndex) {
     }
 }
 
+function getColorByNumber(number) {
+    var colorValue = {
+        0:0x00FF00,
+        2:0xFF0000,
+        4:0x0000FF
+    };
+
+    return colorValue[number];
+}
+
 var rowIndex = generateRandomNumber();
 var columnIndex = generateRandomNumber();
 
@@ -68,6 +77,37 @@ drawCell(rowIndex, columnIndex);
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight'){
-        console.log(event);
+       moveCellToRight();
+       flushUI();
     }
-})
+});
+
+function moveCellToRight() {
+    for (var rowIndex = 0; rowIndex < 4; rowIndex++){
+        for (var columnIndex = 2; columnIndex >= 0; columnIndex--){
+            if (grid[rowIndex][columnIndex] === 0)
+                continue;
+
+            var theEmptyCellIndex = findTheFirstRightCell(rowIndex,columnIndex);
+            if (theEmptyCellIndex !== -1){
+                grid[rowIndex][theEmptyCellIndex] = grid[rowIndex][columnIndex];
+                grid[rowIndex][columnIndex] = 0;
+
+                if (grid[rowIndex][theEmptyCellIndex] === grid[rowIndex][theEmptyCellIndex + 1]){
+                    grid[rowIndex][theEmptyCellIndex] += grid[rowIndex][theEmptyCellIndex + 1];
+                    grid[rowIndex][columnIndex] = 0;
+                }
+            }
+        }
+    }
+}
+
+function findTheFirstRightCell(rowIndex, columnIndex) {
+    for (let i = 3; i > columnIndex; i--) {
+        if (grid[rowIndex][i] === 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
